8fe3adca7c0c7f669f81540a220935b62c0fac82
osztenkurden <hubertwalczak8@gmail.com>
2020-03-01 15:04:53 +0100

    Fixed decider picking

--- src/components/Content/Tabs/Match/SingleVeto.tsx
+++ src/components/Content/Tabs/Match/SingleVeto.tsx
@@ -11,15 +11,18 @@ interface Props {
     onSave: (name: string, map: number) => void;
 }
 
-function generateDescription(veto: I.Veto, team?: I.Team, secTeam?: I.Team){
-    if (!team || !team.name || !veto.mapName) {
+function generateDescription(veto: I.Veto, team?: I.Team, secTeam?: I.Team) {
+    if(!veto.mapName){
         return "";
     }
-    if(veto.type === "decider"){
+    if (veto.type === "decider") {
         return `${veto.mapName} decider`
     }
+    if (!team || !team.name) {
+        return "";
+    }
     let text = `${team.name} ${veto.type}s ${veto.mapName}`;
-    if (secTeam && secTeam.name && veto.side !== "NO"){
+    if (secTeam && secTeam.name && veto.side !== "NO") {
         text += `, ${secTeam.name} chooses ${veto.side} side`;
     }
     return text;
@@ -29,26 +32,26 @@ class SingleVeto extends React.Component<Props> {
     state = {
         isOpen: false
     }
-	toggle = () => {
-		this.setState({ isOpen: !this.state.isOpen });
+    toggle = () => {
+        this.setState({ isOpen: !this.state.isOpen });
     }
-    componentDidMount(){
+    componentDidMount() {
 
     }
     render() {
         const team = this.props.teams.filter(team => team._id === this.props.veto.teamId)[0];
         const secTeam = this.props.teams.filter(team => team._id !== this.props.veto.teamId)[0];
         return (
-                    <div className={`veto-container ${this.props.veto.teamId === "" ? "empty":""} ${this.props.veto.teamId ? this.props.veto.type : ""}`}>
-                        {
-                            this.props.teams.length !== 2 ? "Pick both teams to set vetos" : 
-                            <>
-                                <div className="veto-main">
-                                    <div className="veto-title">VETO {this.props.map+1}:</div>
-                                    <div className="veto-summary">{generateDescription(this.props.veto, team, secTeam)}</div>
-                                    <Button onClick={this.toggle} className="edit-veto purple-btn">Edit</Button>
-                                </div>
-                               {/* <div className="veto-desc">
+            <div className={`veto-container ${this.props.veto.teamId === "" ? "empty" : ""} ${this.props.veto.teamId ? this.props.veto.type : ""}`}>
+                {
+                    this.props.teams.length !== 2 ? "Pick both teams to set vetos" :
+                        <>
+                            <div className="veto-main">
+                                <div className="veto-title">VETO {this.props.map + 1}:</div>
+                                <div className="veto-summary">{generateDescription(this.props.veto, team, secTeam)}</div>
+                                <Button onClick={this.toggle} className="edit-veto purple-btn">Edit</Button>
+                            </div>
+                            {/* <div className="veto-desc">
                                     { score ? <div className="score-container">
                                         <div className={`left-team`}>
                                         <span className="team-name"><TeamLogoName team={team}/></span>
@@ -61,12 +64,12 @@ class SingleVeto extends React.Component<Props> {
                                         </div>
                                     </div> : ''}
                                 </div>*/}
-                                
-                                <VetoModal map={this.props.map} veto={this.props.veto} teams={this.props.teams} isOpen={this.state.isOpen} toggle={this.toggle} onChange={this.props.onSave}/>
-                            </>
-                        }
-                        
-                    </div>
+
+                            <VetoModal map={this.props.map} veto={this.props.veto} teams={this.props.teams} isOpen={this.state.isOpen} toggle={this.toggle} onChange={this.props.onSave} />
+                        </>
+                }
+
+            </div>
         );
     }
 }
