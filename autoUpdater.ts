7e16fa4eee43c9fbd54642e6470913fcf3a7a046
Hubert Walczak <hubertwalczak8@gmail.com>
2021-01-10 17:42:07 +0100

    Lint

--- autoUpdater.ts
+++ autoUpdater.ts
@@ -5,20 +5,20 @@ autoUpdater.autoDownload = false;
 autoUpdater.autoInstallOnAppQuit = false;
 
 export default (window: BrowserWindow) => {
-    autoUpdater.on('update-available', () => {
-        window.webContents.send("updateStatus", true);
-    });
-    autoUpdater.on('update-not-available', () => {
-        window.webContents.send("updateStatus", false);
-    });
+	autoUpdater.on('update-available', () => {
+		window.webContents.send('updateStatus', true);
+	});
+	autoUpdater.on('update-not-available', () => {
+		window.webContents.send('updateStatus', false);
+	});
 
-    autoUpdater.on('update-downloaded', autoUpdater.quitAndInstall);
+	autoUpdater.on('update-downloaded', autoUpdater.quitAndInstall);
 
-    ipcMain.on('updateApp', () => {
-        autoUpdater.downloadUpdate();
-    });
+	ipcMain.on('updateApp', () => {
+		autoUpdater.downloadUpdate();
+	});
 
-    ipcMain.on('checkUpdate', () => {
-        autoUpdater.checkForUpdates();
-    });
-}
+	ipcMain.on('checkUpdate', () => {
+		autoUpdater.checkForUpdates();
+	});
+};
