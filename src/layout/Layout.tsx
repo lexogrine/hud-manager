b396797c3773caea096b253233981bd6eca311f0
Hubert Walczak <hubertwalczak8@gmail.com>
2022-07-06 17:41:53 +0200

    lint

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -165,7 +165,7 @@ export default class Layout extends Component<{}, IState> {
 			const { session, ...user } = appLoadedUser;
 			this.setUser(user);
 			let forceInit = false;
-			if(user.customer && user.workspaces?.length === 1){
+			if (user.customer && user.workspaces?.length === 1) {
 				forceInit = true;
 			}
 			return this.setState({ loading: false }, async () => {
